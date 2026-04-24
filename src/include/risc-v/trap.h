6408999fc43c1576e1862797bd1bf5365f39de3e
krakenlake <tews@airpark.de>
2026-04-24 09:30:56 +0200

    comment

---
+++ src/include/risc-v/trap.h
@@ -0,0 +1,13 @@
+#ifndef PLUTONIC_RISCV_H
+#define PLUTONIC_RISCV_H
+
+
+// sstatus bits
+#define S_MODE_IRQ_FLAG		0b0010
+
+// sie bits
+#define	IRQ_TIMER			0b100000
+
+
+
+#endif /* PLUTONIC_RISCV_H */

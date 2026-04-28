f17cecbc264410d97df219f67706209fcafda3ad
krakenlake <tews@airpark.de>
2026-04-28 21:11:38 +0200

    removed CH32

--- kernel/include/plutonic/drivers/uart/ns16550.h
+++ kernel/include/plutonic/drivers/uart/ns16550.h
@@ -33,17 +33,6 @@
 	#define UART_MODE_8N1   0b00000011
 #endif
 
-#ifdef HW_CH32V003
-	#define UART_BASE		0x40013800
-	#define UART_REG_RDR	0x00
-	#define UART_REG_THR	0x00
-	#define UART_REG_IER	2
-	#define UART_REG_LSR	5
-
-	#define UART_IRQ_ON		0b00000001
-
-	#define UART_MODE_8N1   0b00000011
-#endif
 
 
 #endif /* PLUTONIC_UART_H */

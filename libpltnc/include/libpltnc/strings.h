752b2c5686d5913d4a938ce1a36c62f004982075
krakenlake <tews@airpark.de>
2026-05-04 17:38:05 +0200

    rename

--- libpltnc/include/libpltnc/strings.h
+++ libpltnc/include/libpltnc/strings.h
@@ -5,11 +5,11 @@
  *
  */
 
-#ifndef LIBPLTNC_STRING_H
-#define LIBPLTNC_STRING_H
+#ifndef LIBPLTNC_STRINGS_H
+#define LIBPLTNC_STRINGS_H
 
 #ifndef __ASSEMBLER__
 	extern char* strchain(char *str1, char *str2);
 #endif
 
-#endif /* LIBPLTNC_STRING_H */
+#endif /* LIBPLTNC_STRINGS_H */

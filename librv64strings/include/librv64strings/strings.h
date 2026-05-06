efae6fb4d84fc6f9cef6fe9c5aadfe746ccb2a69
krakenlake <tews@airpark.de>
2026-05-06 15:00:14 +0200

    adding constants.S

--- librv64strings/include/librv64strings/strings.h
+++ librv64strings/include/librv64strings/strings.h
@@ -6,6 +6,9 @@
  */
 #pragma once
 
+#define	HAS_ZERO_BYTE_MAGIC_1	0x0101010101010101
+#define	HAS_ZERO_BYTE_MAGIC_2	0x8080808080808080
+
 #ifndef __ASSEMBLER__
 	extern u64		strlen(char *str);
 	extern char*	strchain(char *str1, char *str2);

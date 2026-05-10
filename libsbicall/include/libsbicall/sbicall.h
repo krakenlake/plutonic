f97e810d48f3c8d5d24979d15d9cb9d553ab41fd
krakenlake <tews@airpark.de>
2026-05-10 12:22:56 +0200

    forward declarations complete

--- libsbicall/include/libsbicall/sbicall.h
+++ libsbicall/include/libsbicall/sbicall.h
@@ -456,6 +456,7 @@
 					unsigned long arg4, unsigned long arg5,
 					long fid, long eid);
 
+
 	/* BASE */
 	struct sbiret sbi_get_spec_version(void);
 	struct sbiret sbi_get_impl_id(void);
@@ -473,46 +474,187 @@
 	long legacy_sbi_send_ipi(const unsigned long *hart_mask);
 	long legacy_sbi_remote_fence_i(const unsigned long *hart_mask);
 	long legacy_sbi_remote_sfence_vma(const unsigned long *hart_mask, 
-		unsigned long start, unsigned long size);
+									unsigned long start,
+									unsigned long size);
 	long legacy_sbi_remote_sfence_vma_asid(const unsigned long *hart_mask,
-		unsigned long start, unsigned long size, unsigned long asid);
+									unsigned long start,
+									unsigned long size,
+									unsigned long asid);
 
 	/* TIME */
 	struct sbiret sbi_set_timer(uint64_t stime_value);
 
 	/* IPI */
 	struct sbiret sbi_send_ipi(unsigned long hart_mask,
-		unsigned long hart_mask_base);
+							unsigned long hart_mask_base);
 
 	/* RFNC */
 	struct sbiret sbi_remote_fence_i(unsigned long hart_mask,
-		unsigned long hart_mask_base);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-			/* DBCN extension */
-	struct sbiret sbi_debug_console_write(unsigned long num_bytes,
-		unsigned long base_addr_lo, unsigned long base_addr_hi);
-	struct sbiret sbi_debug_console_read(unsigned long num_bytes,
-		unsigned long base_addr_lo, unsigned long base_addr_hi);
-	struct sbiret sbi_debug_console_write_byte(uint8_t byte);
+									unsigned long hart_mask_base);
+	struct sbiret sbi_remote_sfence_vma(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size);
+	struct sbiret sbi_remote_sfence_vma_asid(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size,
+									unsigned long asid);
+	struct sbiret sbi_remote_hfence_gvma_vmid(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size,
+									unsigned long vmid);
+	struct sbiret sbi_remote_hfence_gvma(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size);
+	struct sbiret sbi_remote_hfence_vvma_asid(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size,
+									unsigned long asid);
+	struct sbiret sbi_remote_hfence_vvma(unsigned long hart_mask,
+									unsigned long hart_mask_base,
+									unsigned long start_addr,
+									unsigned long size);
+
+	/* HSM */
+	struct sbiret sbi_hart_start(unsigned long hartid,
+							unsigned long start_addr,
+							unsigned long opaque);	
+	struct sbiret sbi_hart_stop(void);
+	struct sbiret sbi_hart_get_status(unsigned long hartid);
+	struct sbiret sbi_hart_suspend(uint32_t suspend_type,
+							unsigned long resume_addr,
+							unsigned long opaque);
+
+	/* SRST */
+	struct sbiret sbi_system_reset(uint32_t reset_type, uint32_t reset_reason);
 
 	/* PMU */
+	struct sbiret sbi_pmu_num_counters(void);
+	struct sbiret sbi_pmu_counter_get_info(unsigned long counter_idx);
+	struct sbiret sbi_pmu_counter_config_matching(unsigned long counter_idx_base,
+									unsigned long counter_idx_mask,
+									unsigned long config_flags,
+									unsigned long event_idx,
+									uint64_t event_data);
+	struct sbiret sbi_pmu_counter_start(unsigned long counter_idx_base,
+									unsigned long counter_idx_mask,
+									unsigned long start_flags,
+									uint64_t initial_value);
+	struct sbiret sbi_pmu_counter_stop(unsigned long counter_idx_base,
+									unsigned long counter_idx_mask,
+									unsigned long stop_flags);
+	struct sbiret sbi_pmu_counter_fw_read(unsigned long counter_idx);
+	struct sbiret sbi_pmu_counter_fw_read_hi(unsigned long counter_idx);
+	struct sbiret sbi_pmu_snapshot_set_shmem(unsigned long shmem_phys_lo,
+									unsigned long shmem_phys_hi,
+									unsigned long flags);
+	struct sbiret sbi_pmu_event_get_info(unsigned long shmem_phys_lo,
+									unsigned long shmem_phys_hi,
+									unsigned long num_entries,
+									unsigned long flags);
+
+	/* DBCN */
+	struct sbiret sbi_debug_console_write(unsigned long num_bytes,
+									unsigned long base_addr_lo, 
+									unsigned long base_addr_hi);
+	struct sbiret sbi_debug_console_read(unsigned long num_bytes,
+									unsigned long base_addr_lo, 
+									unsigned long base_addr_hi);
+	struct sbiret sbi_debug_console_write_byte(uint8_t byte);
+
+	/* SUSP */
+	struct sbiret sbi_system_suspend(uint32_t sleep_type,
+									unsigned long resume_addr,
+									unsigned long opaque);
+
+	/* CPPC */
+	struct sbiret sbi_cppc_probe(uint32_t cppc_reg_id);
+	struct sbiret sbi_cppc_read(uint32_t cppc_reg_id);
+	struct sbiret sbi_cppc_read_hi(uint32_t cppc_reg_id);
+	struct sbiret sbi_cppc_write(uint32_t cppc_reg_id, uint64_t val);
+
+	/* NACL */
+	struct sbiret sbi_nacl_probe_feature(uint32_t feature_id);
+	struct sbiret sbi_nacl_set_shmem(unsigned long shmem_phys_lo,
+									unsigned long shmem_phys_hi,
+									unsigned long flags);
+	struct sbiret sbi_nacl_sync_csr(unsigned long csr_num);
+	struct sbiret sbi_nacl_sync_hfence(unsigned long entry_index);
+	struct sbiret sbi_nacl_sync_sret(void);
+
+	/* STA */
+	struct sbiret sbi_steal_time_set_shmem(unsigned long shmem_phys_lo,
+									unsigned long shmem_phys_hi,
+									unsigned long flags);
+
+	/* SSE */
+	struct sbiret sbi_sse_read_attrs(uint32_t event_id,
+									uint32_t base_attr_id, 
+									uint32_t attr_count,
+									unsigned long output_phys_lo,
+									unsigned long output_phys_hi);
+	struct sbiret sbi_sse_write_attrs(uint32_t event_id,
+									uint32_t base_attr_id, 
+									uint32_t attr_count,
+									unsigned long input_phys_lo,
+									unsigned long input_phys_hi);
+	struct sbiret sbi_sse_register(uint32_t event_id,
+									unsigned long handler_entry_pc,
+									unsigned long handler_entry_arg);
+	struct sbiret sbi_sse_unregister(uint32_t event_id);
+	struct sbiret sbi_sse_enable(uint32_t event_id);
+	struct sbiret sbi_sse_disable(uint32_t event_id);
+	struct sbiret sbi_sse_complete(void);
+	struct sbiret sbi_sse_inject(uint32_t event_id, unsigned long hart_id);
+	struct sbiret sbi_sse_hart_unmask(void);
+	struct sbiret sbi_sse_hart_mask(void);
+
+	/* FWFT */
+	struct sbiret sbi_fwft_set(uint32_t feature,
+							unsigned long value,
+							unsigned long flags);
+	struct sbiret sbi_fwft_get(uint32_t feature);
+
+	/* DBTR */
+	struct sbiret sbi_debug_num_triggers(unsigned long trig_tdata1);
+	struct sbiret sbi_debug_set_shmem(unsigned long shmem_phys_lo,
+										unsigned long shmem_phys_hi,
+										unsigned long flags);
+	struct sbiret sbi_debug_read_triggers(unsigned long trig_idx_base,
+										unsigned long trig_count);
+	struct sbiret sbi_debug_install_triggers(unsigned long trig_count);
+	struct sbiret sbi_debug_update_triggers(unsigned long trig_count);
+	struct sbiret sbi_debug_uninstall_triggers(unsigned long trig_idx_base,
+										unsigned long trig_idx_mask);
+	struct sbiret sbi_debug_enable_triggers(unsigned long trig_idx_base,
+										unsigned long trig_idx_mask);
+	struct sbiret sbi_debug_disable_triggers(unsigned long trig_idx_base,
+										unsigned long trig_idx_mask);
+
+	/* MPXY*/	
+	struct sbiret sbi_mpxy_get_shmem_size(void);
+	struct sbiret sbi_mpxy_set_shmem(unsigned long shmem_phys_lo,
+									unsigned long shmem_phys_hi,
+									unsigned long flags);
+	struct sbiret sbi_mpxy_get_channel_ids(uint32_t start_index);
+	struct sbiret sbi_mpxy_read_attributes(uint32_t channel_id,
+									uint32_t base_attribute_id,
+									uint32_t attribute_count);
+	struct sbiret sbi_mpxy_write_attributes(uint32_t channel_id,
+									uint32_t base_attribute_id,
+									uint32_t attribute_count);
+	struct sbiret sbi_mpxy_send_message_with_response(uint32_t channel_id,
+									uint32_t message_id,
+									unsigned long message_data_len);
+	struct sbiret sbi_mpxy_send_message_without_response(uint32_t channel_id,
+									uint32_t message_id,
+									unsigned long message_data_len);
+	struct sbiret sbi_mpxy_get_notification_events(uint32_t channel_id);
+
 
 	#if (XLEN == 32)
 		struct sbi_event_idx_code {

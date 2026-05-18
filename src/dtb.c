4aadecc258aea44a470e7cce6af21c18cb1a8608
krakenlake <tews@airpark.de>
2026-05-18 15:25:27 +0200

    fixed checking of magic number

--- src/dtb.c
+++ src/dtb.c
@@ -24,7 +24,7 @@ long print_dtb(void)
 		return ERR_NOTFOUND;
 	}
 
-	u32 magic_le = swap32(dtb->magic);
+	u64 magic_le = swap32((u32)dtb->magic);
 	log(LOG_DEBUG, "dtb magic LITTLE_ENDIAN = 0x%08x", magic_le);
 
 	if (magic_le == FDT_HEADER_MAGIC) {
@@ -36,11 +36,17 @@ long print_dtb(void)
 
 	u64 strings_offset = (u64)swap32(dtb->off_dt_strings);
 	u64 strings_start_addr = (u64)dtb + strings_offset;
-	log(LOG_DEBUG, "strings section @0x%08x, starting with \"%s\"", strings_start_addr, strings_start_addr);
+	u64 strings_end_addr = strings_start_addr +  (u64)swap32(dtb->size_dt_strings);
+	log(LOG_DEBUG, "strings section @0x%08x-0x%08x (%ld bytes), starting with \"%s\"", 
+					strings_start_addr, strings_end_addr, 
+					(u64)swap32(dtb->size_dt_strings), strings_start_addr);
 
 	u64 struct_offset = (u64)swap32(dtb->off_dt_struct);
 	u64 struct_start_addr = (u64)dtb + struct_offset;
-	log(LOG_DEBUG, "struct section @0x%08x", struct_start_addr);
+	u64 struct_end_addr = struct_start_addr +  (u64)swap32(dtb->size_dt_struct);
+	log(LOG_DEBUG, "struct section @0x%08x-0x%08x (%ld bytes)", 
+		struct_start_addr, struct_end_addr,
+		(u64)swap32(dtb->size_dt_struct));
 
 	u64 mem_rsvmap_offset = (u64)swap32(dtb->off_mem_rsvmap);
 	struct fdt_reserve_entry *mem_rsvmap_start_addr = (struct fdt_reserve_entry*)(dtb + mem_rsvmap_offset);
